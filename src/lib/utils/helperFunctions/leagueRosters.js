65226723e23d20ddd625aa35495b4081540bf57b
Luke Toenjes <luke.toenjes@hotmail.com>
2023-01-11 22:03:31 -0700

    fix: ensure rosters store is always sorted

--- src/lib/utils/helperFunctions/leagueRosters.js
+++ src/lib/utils/helperFunctions/leagueRosters.js
@@ -4,7 +4,10 @@ import {rostersStore} from '$lib/stores';
 
 export const getLeagueRosters = async (queryLeagueID = leagueID) => {
 	if(get(rostersStore)[queryLeagueID]) {
-		return get(rostersStore)[queryLeagueID];
+		const r = get(rostersStore)[queryLeagueID];		
+		r.rosters.sort((a, b) => a.roster_id - b.roster_id);
+		
+		return r;
 	}
     const res = await fetch(`https://api.sleeper.app/v1/league/${queryLeagueID}/rosters`, {compress: true}).catch((err) => { console.error(err); });
 	const data = await res.json().catch((err) => { console.error(err); });
